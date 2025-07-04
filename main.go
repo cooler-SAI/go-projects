90de9345d29ffae30989cd9bdfbdd0336ce59e44
cooler-SAI <coolerme86@gmail.com>
2025-07-04 21:48:30 +0300

    clean up

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"github.com/cooler-SAI/go-Tools/zerolog"
-)
-
 func main() {
 
-	zerolog.Init()
-
-	zerolog.Log.Error().Msg("Hello World")
-	zerolog.Log.Info().Msg("Testing")
 }
