3b1b51764f49710bbe8ad6e10328c05ff2904b6e
cooler-SAI <coolerme86@gmail.com>
2025-08-17 00:13:26 +0300

    feat(concurrency): Demonstrate race condition and mutex solution
    Adds a new example, `mutex2.go`, to demonstrate a classic race
    condition when multiple goroutines access a shared variable
    concurrently.
    
    - The `withoutMutex` function shows the unpredictable and incorrect
      result of incrementing a shared counter without synchronization.
    - The `withMutex` function provides the solution, using a `sync.Mutex`
      to protect the critical section, ensuring the final counter value
      is correct and the operation is atomic.
    
    This example serves as a clear illustration of the necessity of
    mutexes for safe concurrent programming in Go.

---
+++ mutex/mutex2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var counter int
+
+func withoutMutex() {
+
+	var wg sync.WaitGroup
+	numGoroutines := 1000
+
+	fmt.Printf("Running demo without a mutex. Launching %d goroutines.\n", numGoroutines)
+	wg.Add(numGoroutines)
+
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			defer wg.Done()
+			counter++
+		}()
+	}
+
+	// Ждем, пока все горутины завершатся
+	wg.Wait()
+	fmt.Printf("Final counter value: %d\n", counter)
+	fmt.Println("Expected value: 1000")
+	fmt.Println("This is a classic race condition. The final value is not 1000 because " +
+		"multiple goroutines are trying to increment the counter at the same time.")
+}
+
+func withMutex() {
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	counter := 0
+	numGoroutines := 1000
+
+	fmt.Printf("\nRunning demo with a mutex. Launching %d goroutines.\n", numGoroutines)
+	wg.Add(numGoroutines)
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			defer wg.Done()
+			mu.Lock()
+			counter++
+			mu.Unlock()
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("Final counter value: %d\n", counter)
+	fmt.Println("Expected value: 1000")
+	fmt.Println("The final value is correct because the mutex ensures that only one goroutine at a time can " +
+		"access and modify the counter.")
+
+}
+
+func main() {
+	withoutMutex()
+	time.Sleep(1 * time.Second)
+	withMutex()
+}
